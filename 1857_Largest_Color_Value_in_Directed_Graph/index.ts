8c5b2461a46017ecae4a7404be11c5432f214259
textyre <uliyan96@gmail.com>
2023-08-23 19:21:32 +0300

    26 leetcode

--- 1857_Largest_Color_Value_in_Directed_Graph/index.ts
+++ 1857_Largest_Color_Value_in_Directed_Graph/index.ts
@@ -1,13 +1,48 @@
 function largestPathValue(colors: string, edges: number[][]): number {
-  const adj = {};
+  const graph: {
+    [x: number]: number[];
+  } = {};
+  const indegree = new Array(colors.length).fill(0);
+
   for (let [from, to] of edges) {
-    adj[from] = {
-      [to]: to,
-      ...adj[from],
-    };
+    if (from === to) return -1;
+    graph[from] = [...(graph[from] || []), to];
+    indegree[to]++;
   }
-  console.log(adj);
-  return 0;
+
+  const queue: number[] = [];
+  for (let i = 0; i < colors.length; i++) {
+    if (indegree[i] === 0) queue.push(i);
+  }
+
+  let maxColor = 0;
+  let nodesOfColors = Array.from({ length: colors.length }, () =>
+    Array(26).fill(0)
+  );
+
+  while (queue.length) {
+    const vertex = queue.shift();
+    const color = colors.charCodeAt(vertex) - 97;
+    nodesOfColors[vertex][color]++;
+    maxColor = Math.max(maxColor, nodesOfColors[vertex][color]);
+
+    if (!graph[vertex]) continue;
+
+    for (let neighbor of graph[vertex]) {
+      indegree[neighbor]--;
+
+      for (let i = 0; i < 26; i++) {
+        nodesOfColors[neighbor][i] = Math.max(
+          nodesOfColors[neighbor][i],
+          nodesOfColors[vertex][i]
+        );
+      }
+
+      if (indegree[neighbor] === 0) queue.push(neighbor);
+    }
+  }
+
+  return indegree.some((x) => x > 0) ? -1 : maxColor;
 }
 
 const colors = 'abaca';
@@ -16,7 +51,6 @@ const edges = [
   [0, 2],
   [2, 3],
   [3, 4],
-  [3, 5],
 ];
 
-largestPathValue(colors, edges);
+console.log(largestPathValue(colors, edges));
